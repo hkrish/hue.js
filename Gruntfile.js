c3f60b84e4bf6817cb67a8322632800708c3b687
hkrish <hari.exeption@gmail.com>
2014-01-02 10:38:50 +0100

    Ignore banners in built files

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                // banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
                 src: 'src/hue.js',
