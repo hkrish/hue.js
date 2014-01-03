f6fd9949f53508ca002c227d4a8a5b2ca977e056
hkrish <hari.exeption@gmail.com>
2014-01-03 13:15:23 +0100

    Find most saturated color for a given hue

--- src/hue.js
+++ src/hue.js
@@ -18,11 +18,13 @@ var Hue = (function () {
     var _RefWhite = "D65",
         _RefRGB   = "sRGB",
         // Constants
+        // Un and Vn and the u, v coordinates of the reference white
         Un = 0.19783982482140777,
         Vn = 0.4683363029324097,
+        // More accurate representation of constants as rational numbers for 
+        // the CIE XYZ <-> LUV conversions
         kE = 216 / 24389,
         kK = 24389 / 27,
-        PIover180 = Math.PI / 180,
         // RGB <-> XYZ conversion matrices
         // See http://www.brucelindbloom.com/Eqn_RGB_XYZ_Matrix.html for
         // original formulae
@@ -95,6 +97,7 @@ var Hue = (function () {
         var x = +xyz[0],
             y = +xyz[1],
             z = +xyz[2],
+            exp = Math.exp, log = Math.log,
             den = x + 15.0 * y + 3 * z,
             u = den > 0 ? 4.0 * x / den : 0.0,
             v = den > 0 ? 9.0 * y / den : 0.0,
@@ -129,6 +132,7 @@ var Hue = (function () {
     function _LUVtoLCH(luv) {
         var u = luv[1],
             v = luv[2],
+            PIover180 = Math.PI / 180,
             h = Math.atan2(v, u) / PIover180;
 
         luv[1] = Math.sqrt(u * u + v * v);
@@ -139,12 +143,94 @@ var Hue = (function () {
 
     function _LCHtoLUV(lch) {
         var c = lch[1],
+            PIover180 = Math.PI / 180,
             h = lch[2] * PIover180;
 
         lch[1] = c * Math.cos(h);
         lch[2] = c * Math.sin(h);
 
         return lch;
+    }
+
+    /**
+     * Convert a given Hue angle to the most saturated RGB color in that hue.
+     * 
+     * @param  {Number} hue The hue angle (in LUV or LCHuv) in degrees
+     *                      in the range 0 - 360.
+     * @return {Array}      Most saturated color as an [R, G, B] array
+     */
+    function hueMSC (hue) {
+        /*
+         * Hue corresponding to colours at the corners of an RGB cube, and the
+         * min and max RGB components throughout that edge
+         *  (0-Red, 1-Green, 2-Blue).
+         * These values correspond to sRGB and D65 white point
+         */
+        var rgbPrimariesH = [   
+                12.1740,        // H in LCHuv for Red
+                85.8727,        // Yellow
+                127.7236,       // Green
+                192.1740,       // Cyan
+                265.8727,       // Blue
+                307.7236 ];     // Magenta
+        var rgbEdges = [
+                [rgbPrimariesH[0], rgbPrimariesH[1], 1, 2, 0],
+                [rgbPrimariesH[1], rgbPrimariesH[2], 0, 2, 1],
+                [rgbPrimariesH[2], rgbPrimariesH[3], 2, 0, 1],
+                [rgbPrimariesH[3], rgbPrimariesH[4], 1, 0, 2],
+                [rgbPrimariesH[4], rgbPrimariesH[5], 0, 1, 2],
+                [rgbPrimariesH[5], rgbPrimariesH[0], 2, 1, 0] ];
+        // The formulae used here is referenced from Martijn Wijffelaars's 
+        // Master's theses
+        //  http://alexandria.tue.nl/extra2/afstversl/wsk-i/wijffelaars2008.pdf
+        //  
+        // I have changed the formula slightly to use the more complicated but
+        // accurate sRGB gamma correction, which is used consistently throughout
+        // the library.
+        var msc = [0, 0, 0], variant, min, max, i, edge,
+            mrX, mrY, mrZ, mtX, mtY, mtZ, s = 1, cRp,
+            PIover180 = Math.PI / 180,
+            a = - Math.sin(hue * PIover180),
+            b = Math.cos(hue * PIover180),
+            exp = Math.exp, log = Math.log;
+        // Find the edge in RGB cube for the given hue
+        for (i = 0; i < 6; i++) {
+            edge = rgbEdges[i];
+            if (edge[0] <= hue && hue < edge[1]) {
+                break;
+            }
+        }
+        variant = edge[2];
+        min = edge[3];
+        max = edge[4];
+        msc[min] = 0;
+        msc[max] = 1;
+        max *= 3;
+        mrX = mRGBtoXYZ[variant * 3];
+        mrY = mRGBtoXYZ[variant * 3 + 1];
+        mrZ = mRGBtoXYZ[variant * 3 + 2];
+        mtX = mRGBtoXYZ[max];
+        mtY = mRGBtoXYZ[max + 1];
+        mtZ = mRGBtoXYZ[max + 2];
+        // Calculate the linear variant value r, g or b
+        cRp =  - ((a*Un + b*Vn) * (mtX + 15*mtY + 3*mtZ) - (4*a*mtX + 9*b*mtY)) /
+                ((a*Un + b*Vn) * (mrX + 15*mrY + 3*mrZ) - (4*a*mrX + 9*b*mrY));
+        // sRGB gamma correction from linear cRp
+        if (cRp < 0) {
+            s = -1;
+            cRp = -cRp;
+        }
+        if (cRp <= 0.0031308) {
+            cRp = cRp * 12.92;
+        } else {
+            cRp = 1.055 * exp((1/2.4) * log(cRp)) - 0.055;
+        }
+        msc[variant] = s * cRp;
+        // Scale RGB values ane return
+        for (i = 0; i < 3; i++) {
+            msc[i] = (msc[i] * 255 + 0.5) | 0;
+        }
+        return msc;
     }
 
 
@@ -178,6 +264,8 @@ var Hue = (function () {
     Hue.LCHtoRGB = function(lch){
         return _XYZtoRGB(_LUVtoXYZ(_LCHtoLUV(lch)));
     };
+    // Utility functions
+    Hue.hueMSC = hueMSC;
 
     return Hue;
 }());
